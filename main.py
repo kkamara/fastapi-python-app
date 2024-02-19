1fca125f10fe936322d1515a8341a5ea7a2ae9c1
kkamara <kamaracomputers@gmail.com>
2024-02-19 19:25:47 +0000

    Handle request errors 2 (better alternative)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Body, FastAPI, Response, status
+from fastapi import Body, FastAPI, status, HTTPException
 from pydantic import BaseModel
 from random import randrange
 
@@ -34,9 +34,9 @@ def create_posts(post: Post):
     return {"data": post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
+def get_post(id: int):
     post = find_post(id)
     if not post:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"message": f"Post with id {id} was not found."}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} was not found.")
     return {"post_detail": post}
